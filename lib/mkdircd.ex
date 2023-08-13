234ac24b797ef741439baac9d6057e9c6a861890
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2023-08-13 23:19:03 +0900

    mix new .

---
+++ lib/mkdircd.ex
@@ -0,0 +1,18 @@
+defmodule Mkdircd do
+  @moduledoc """
+  Documentation for `Mkdircd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mkdircd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mkdircd_test.exs
@@ -0,0 +1,8 @@
+defmodule MkdircdTest do
+  use ExUnit.Case
+  doctest Mkdircd
+
+  test "greets the world" do
+    assert Mkdircd.hello() == :world
+  end
+end
