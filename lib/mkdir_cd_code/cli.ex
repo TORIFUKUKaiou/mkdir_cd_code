3f6642a0bbead9a0a7099f5b2f4d2af8c109a6ff
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2023-08-14 13:59:42 +0900

    update lib/mkdir_cd_code/cli.ex

--- lib/mkdir_cd_code/cli.ex
+++ lib/mkdir_cd_code/cli.ex
@@ -1,5 +1,9 @@
 defmodule MkdirCdCode.CLI do
-  @spec main(nonempty_maybe_improper_list) :: nil | {any, non_neg_integer}
+  @spec main(maybe_improper_list) :: nil | :ok | {any, non_neg_integer}
+  def main([]) do
+    IO.puts("Usage: mkdir_cd_code dir_name")
+  end
+
   def main(args) do
     [dir | _] = args
     :ok = File.mkdir_p(dir)
